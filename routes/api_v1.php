05058138b640db14e1203872382afd21b6c3e526
poljak07 <acmeta4@gmail.com>
2025-01-04 21:27:33 +0100

    Design and Versioning

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
-
-Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
