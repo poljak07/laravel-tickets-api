19f8e71393c3954bf6394807c11c90d3dc4071ab
poljak07 <acmeta4@gmail.com>
2025-01-05 21:43:30 +0100

    Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
-
-Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
