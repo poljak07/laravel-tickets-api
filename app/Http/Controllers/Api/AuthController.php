d6360f2962c6d527cf5028609a447a2901cb6b3e
poljak07 <acmeta4@gmail.com>
2025-01-06 18:33:58 +0100

    Revoking Authentication Tokens

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginUserRequest;
 use App\Traits\ApiResponses;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
@@ -24,9 +25,20 @@ class AuthController extends Controller
         return $this->ok(
           'Authenticated',
           [
-              'token' => $user->createToken('API token for' . $user->email)->plainTextToken
+              'token' => $user->createToken(
+                  'API token for' . $user->email,
+              ['*'],
+              now()->addMonth()
+              )->plainTextToken
           ]
         );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('');
     }
 
 }
