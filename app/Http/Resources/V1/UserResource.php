49377bcdbc4be43b06ef1dad193d612d27ea2255
poljak07 <acmeta4@gmail.com>
2025-01-11 23:34:00 +0100

    Using and Filtering Nested Resources

--- app/Http/Resources/V1/UserResource.php
+++ app/Http/Resources/V1/UserResource.php
@@ -20,14 +20,14 @@ class UserResource extends JsonResource
           'attributes' => [
               'name' => $this->name,
               'email' => $this->email,
-              $this->mergeWhen($request->routeIs('users.*'), [
+              $this->mergeWhen($request->routeIs('authors.*'), [
                   'emailVerifiedAt' => $this->email_verified_at,
                   'createdAt' => $this->created_at,
                   'updatedAt' => $this->updated_at,
               ])
           ],
             'includes' => TicketResource::collection($this->whenLoaded('tickets')),
-            'links' => ['self' => route('users.show', ['user' => $this->id])
+            'links' => ['self' => route('authors.show', ['author' => $this->id])
     ],
         ];
     }
