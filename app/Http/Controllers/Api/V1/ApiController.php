1bef83c3855717d5cbe2dee144b13d04fa4cf350
poljak07 <acmeta4@gmail.com>
2025-01-17 15:15:40 +0100

    User Authorization with Policy

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponses;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+    use AuthorizesRequests;
     use ApiResponses;
+
+    protected $policyClass;
 
     public function include(string $relationship): bool
     {
@@ -21,5 +25,10 @@ class ApiController extends Controller
         $includeValues = explode(',', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+    public function isAble($ability, $targetModel)
+    {
+        return $this->authorize($ability, [$targetModel, $this->policyClass]);
     }
 }
